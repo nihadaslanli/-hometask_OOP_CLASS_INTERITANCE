e850abf3e667f022867cfa52935c5b58f99fdec1
nihadaslanli <nihadsasl@code.edu.az>
2024-02-29 16:50:42 +0400

    Add project files.

---
+++ #hometask_OOP_CLASS_INTERITANCE/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _hometask_OOP_CLASS_INTERITANCE
+{
+    internal class Account
+    {
+
+        public string Usernameadmin;
+        public string Usersurname;
+        public string Usermailadress;
+        public string Userpassword;
+
+
+        public void Gmail()
+        {
+            Console.WriteLine(Usermailadress + "" + Userpassword);
+        }
+    }
+}
---
+++ #hometask_OOP_CLASS_INTERITANCE/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _hometask_OOP_CLASS_INTERITANCE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                #region task1
+                //Account login1 = new Account();
+                //login1.Usernameadmin = "name1";
+                //login1.Usersurname = "surname1";
+                //login1.Usermailadress = "namesurname1@gmail.com";
+                //login1.Userpassword = "skkaskak";
+
+                //if (login1.Usernameadmin == "EA18yr@" && login1.Userpassword == "EA18yr@")
+                //    Console.WriteLine("ugurlu giris");
+                //else
+                //{
+                //    Console.WriteLine("username ve password sehvdir.");
+                //}
+                #endregion
+
+
+
+
+
+
+            }
+        }
+
+    }
+
+}
+
